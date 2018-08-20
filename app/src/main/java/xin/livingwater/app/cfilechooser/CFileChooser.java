2e5f4b7f4ad030268174319ac24c4b7a4fb1aad4
livingwater <livingwater@huoshuiqingniangongzuoshi.local>
2018-08-20 08:42:58 +0800

    Initial commit

---
+++ app/src/main/java/xin/livingwater/app/cfilechooser/CFileChooser.java
@@ -0,0 +1,13 @@
+package xin.livingwater.app.cfilechooser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CFileChooser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cfile_chooser);
+    }
+}
