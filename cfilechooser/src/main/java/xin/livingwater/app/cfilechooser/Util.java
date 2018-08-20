823e368a0e5767570052e505678bfa8e58d36bc7
livingwater <863203434@qq.com>
2018-08-20 21:24:43 +0800

    nothing

---
+++ cfilechooser/src/main/java/xin/livingwater/app/cfilechooser/Util.java
@@ -0,0 +1,10 @@
+package xin.livingwater.app.cfilechooser;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Util {
+    public static void showToast(Context context, String content) {
+        Toast.makeText(context, content, Toast.LENGTH_LONG);
+    }
+}
